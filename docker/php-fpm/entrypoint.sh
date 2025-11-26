1d154513b61c19d9edabb44eb153e354c7d515ea
Marc Schlagenhauf <schlagma@users.noreply.github.com>
2025-11-26 03:40:06 +0100

    Docker development environment (#50)
    * Init docker compose development environment
    
    * Add LDAP config to environment file
    
    * added git, so it builds with podman
    
    * small cleanup
    
    * started cheatsheet / dev readme
    
    * added php zip ext for smoother composer compression
    
    * WIP
    fixed ldap port, ldif path
    no root passwords in .env file
    ports from .env
    
    * basic ldif file
    
    ---------
    
    Co-authored-by: Lukas Staab <l.staab@posteo.de>

---
+++ docker/php-fpm/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Based on https://github.com/dockersamples/laravel-docker-examples
+# (c) 2024 Sergei Shitikov
+# License: https://github.com/dockersamples/laravel-docker-examples/blob/main/LICENSE
+
+set -e
+
+# Check if $UID and $GID are set, else fallback to default (1000:1000)
+USER_ID=${UID:-1000}
+GROUP_ID=${GID:-1000}
+
+# Fix file ownership and permissions using the passed UID and GID
+echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
+chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
+
+# Run database migrations
+php artisan migrate
+
+# Clear configurations to avoid caching issues in development
+echo "Clearing configurations..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+# Run the default command (e.g., php-fpm or bash)
+exec "$@"
