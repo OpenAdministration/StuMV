d6d776e613292e9391722f1a727dbbcbe99f4039
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-13 19:17:19 +0100

    Profile navbar

--- resources/views/components/navbar-profile.blade.php
+++ resources/views/components/navbar-profile.blade.php
@@ -1,6 +1,6 @@
 <div class="border-b border-zinc-200 dark:border-zinc-700">
     <div class="mx-6 sm:mx-8">
-        <div class="max-w-6xl mx-auto -mb-[1px]">
+        <div class="max-w-6xl mx-auto -mb-[1px] overflow-x-auto">
             <flux:navbar>
                 <flux:navbar.item wire:navigate href="{{ route('profile', ['username' => $username]) }}">{{ __('Profile') }}</flux:navbar.item>
                 <flux:navbar.item wire:navigate href="{{ route('profile.picture', ['username' => $username]) }}">{{ __('profile.picture') }}</flux:navbar.item>
