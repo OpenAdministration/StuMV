c3d88a7aaa0237861e3821d85fccd7476de0a49e
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-19 15:56:33 +0100

    auth validation errors as flux callout

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,10 +1,11 @@
 <x-guest-layout>
     <x-auth-card>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
         <flux:card class="grid gap-6 w-full bg-zinc-50 dark:bg-zinc-900 sm:bg-white sm:dark-bg-zinc-800 max-w-[28rem]! mx-auto border-0 sm:border-1">
             <flux:heading size="xl">{{ __('Reset Password') }}</flux:heading>
+
+            <!-- Validation Errors -->
+            <x-auth-validation-errors :errors="$errors" />
+
             <form method="POST" action="{{ route('password.update') }}" class="flex flex-col gap-6">
                 @csrf
 
--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -1,15 +1,18 @@
 @props(['errors'])
 
 @if ($errors->any())
+    <flux:callout variant="danger" icon="circle-x" />
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <flux:callout.heading>
             {{ __('Whoops! Something went wrong.') }}
-        </div>
+        </flux:callout.heading>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <flux:callout.text>
+            <ul class="list-disc list-inside">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </flux:callout.text>
     </div>
 @endif
