9f62d674c1eb768fb58648c4ada9a45236dba794
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-19 15:32:58 +0100

    Use flux card on password reset

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,5 @@
 <x-guest-layout>
     <x-auth-card>
-
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
             <flux:card class="grid gap-6 w-full bg-zinc-50 dark:bg-zinc-900 sm:bg-white sm:dark-bg-zinc-800 max-w-[28rem]! mx-auto border-0 sm:border-1">
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -3,26 +3,28 @@
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-        <h2 class="font-bold text-gray-900 sm:truncate sm:tracking-tight">{{ __('Reset Password') }}</h2>
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+        <flux:card class="grid gap-6 w-full bg-zinc-50 dark:bg-zinc-900 sm:bg-white sm:dark-bg-zinc-800 max-w-[28rem]! mx-auto border-0 sm:border-1">
+            <flux:heading size="xl">{{ __('Reset Password') }}</flux:heading>
+            <form method="POST" action="{{ route('password.update') }}" class="flex flex-col gap-6">
+                @csrf
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-            <input type="hidden" name="mail" value="{{ $request->mail }}">
+                <!-- Password Reset Token -->
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <input type="hidden" name="mail" value="{{ $request->mail }}">
 
-            <!-- Email Address -->
-            <flux:input id="mail" name="mail" :label="__('E-Mail')" :value="old('mail', $request->mail)" required disabled/>
+                <!-- Email Address -->
+                <flux:input id="mail" name="mail" :label="__('E-Mail')" :value="old('mail', $request->mail)" required disabled/>
 
-            <!-- Password -->
-            <flux:input id="password" name="password" :label="__('Password')" type="password" required autofocus/>
-            <flux:input id="password_confirmation" name="password_confirmation" :label="__('Confirm Password')" type="password" required/>
+                <!-- Password -->
+                <flux:input id="password" name="password" :label="__('Password')" type="password" required autofocus/>
+                <flux:input id="password_confirmation" name="password_confirmation" :label="__('Confirm Password')" type="password" required/>
 
-            <div class="flex items-center justify-end mt-6">
-                <flux:button variant="primary" type="submit">
-                    {{ __('Reset Password') }}
-                </flux:button>
-            </div>
-        </form>
+                <div class="flex items-center justify-end mt-6">
+                    <flux:button variant="primary" type="submit">
+                        {{ __('Reset Password') }}
+                    </flux:button>
+                </div>
+            </form>
+        </flux:card>
     </x-auth-card>
 </x-guest-layout>
