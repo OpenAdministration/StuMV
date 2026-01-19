1bdbcb5cc630efa8f441b1077ab8edb968bfb8d8
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-19 16:19:50 +0100

    Ban and save icons

--- resources/views/components/livewire-form.blade.php
+++ resources/views/components/livewire-form.blade.php
@@ -2,10 +2,10 @@
     {{ $slot }}
     <div class="mt-6 flex items-center justify-end gap-x-3">
         @isset($abort_route)
-            <flux:button wire:navigate href="{{ $abort_route }}">{{  __('Cancel') }}</flux:button>
+            <flux:button icon="ban" wire:navigate href="{{ $abort_route }}">{{  __('Cancel') }}</flux:button>
         @else
-            <flux:button wire:navigate href="{{ url()->previous() }}">{{  __('Cancel') }}</flux:button>
+            <flux:button icon="ban" wire:navigate href="{{ url()->previous() }}">{{  __('Cancel') }}</flux:button>
         @endisset
-        <flux:button variant="primary" type="submit">{{ __('Save') }}</flux:button>
+        <flux:button variant="primary" icon="save" type="submit">{{ __('Save') }}</flux:button>
     </div>
 </form>
