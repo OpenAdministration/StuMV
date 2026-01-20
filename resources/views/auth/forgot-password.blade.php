1ee2559693ded85dd2450eaa907c8da2830da97e
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-20 04:24:00 +0100

    lg header

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,7 +3,7 @@
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
             <flux:card class="grid gap-6 w-full bg-zinc-50 dark:bg-zinc-900 sm:bg-white sm:dark-bg-zinc-800 max-w-[28rem]! mx-auto border-0 sm:border-1">
-                <flux:heading size="xl">{{ __('Forgot your password?') }}</flux:heading>
+                <flux:heading size="lg">{{ __('Forgot your password?') }}</flux:heading>
 
                 <flux:text>{{ __('auth.forgot_password_text') }}</flux:text>
 
