2b037677c1c6f9e6d42507dc2211bc6bab7ec72d
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-21 01:47:55 +0100

    move status

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,14 +1,18 @@
 <x-guest-layout>
     <x-auth-card>
+        <!-- Session Status -->
+        @if(session('status'))
+            <div class="w-full max-w-[28rem]!">
+                <x-auth-session-status :status="session('status')" />
+            </div>
+        @endif
+
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
             <flux:card class="grid gap-6 w-full bg-zinc-50 dark:bg-zinc-900 sm:bg-white sm:dark-bg-zinc-800 max-w-[28rem]! mx-auto border-0 sm:border-1 sm:shadow-xs">
                 <flux:heading size="xl">{{ __('Forgot your password?') }}</flux:heading>
 
                 <flux:text>{{ __('auth.forgot_password_text') }}</flux:text>
-
-                <!-- Session Status -->
-                <x-auth-session-status :status="session('status')" />
 
                 <flux:field>
                     <flux:label>{{ __('Email') }}</flux:label>
