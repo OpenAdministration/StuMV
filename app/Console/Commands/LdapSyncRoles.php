1347518972e0a9e4cfa85c479227267938a00bb0
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-30 00:41:27 +0100

    Try to fix deletion of old entries

--- app/Console/Commands/LdapSyncRoles.php
+++ app/Console/Commands/LdapSyncRoles.php
@@ -68,7 +68,7 @@ class LdapSyncRoles extends Command
                         ->get();
                     $this->comment("  |-> " . $role->getDn());
                     // delete all members so far
-                    $role->setAttribute('uniqueMember', ['']);
+                    $role->deleteAttributes($role->getDn(), ['uniqueMember' => []]);
                     $ldapMembers = $role->members();
                     foreach ($activeMemberships as $membership){
                         /** @var RoleMembership $membership */
@@ -91,7 +91,7 @@ class LdapSyncRoles extends Command
                 $this->comment("> " . $group->getDn());
 
                 // delete all members so far
-                $group->setAttribute('uniqueMember', ['']);
+                $group->deleteAttributes($group->getDn(), ['uniqueMember' => []]);
 
                 $roles = GroupMembership::where('group_dn', $group->getDn())->get();
                 
