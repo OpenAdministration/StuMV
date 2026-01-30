ac7a570a4c571c179d17e8b1c5d202671e4e2bf2
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-30 02:26:24 +0100

    Add attribute name to add

--- app/Console/Commands/LdapSyncRoles.php
+++ app/Console/Commands/LdapSyncRoles.php
@@ -75,7 +75,7 @@ class LdapSyncRoles extends Command
 
                     // delete all members so far
                     $currentMembers = $role->getAttribute('uniqueMember');
-                    $query->add($role->getDn(), ['']);
+                    $query->add($role->getDn(), ['uniqueMember' => '']);
                     for ($i = 0; $i < count($currentMembers); $i++) {
                         if ($currentMembers[$i] !== '') {
                             $query->remove($role->getDn(), ['uniqueMember' => [ $currentMembers[$i] ]]);
