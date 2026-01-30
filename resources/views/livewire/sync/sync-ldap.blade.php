7eaecec5fdcfbe9c44f98f7db529026127baf922
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-30 19:26:27 +0100

    Another try

--- resources/views/livewire/sync/sync-ldap.blade.php
+++ resources/views/livewire/sync/sync-ldap.blade.php
@@ -1,10 +1,10 @@
 <div>
-    @if(auth()->user()->can('superadmin'))
+    @can('superadmin', \App\Models\User::class)
         <flux:button
             variant="ghost"
             icon="folder-sync"
             wire:click="sync"
             title="__('sync.ldap_title')"
         />
-    @endif
+    @endcan
 </div>
