e5c8f76766d70518ff14a83c88a0ea79e3fb6cad
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-30 17:22:35 +0100

    w-full in edit committee view

--- resources/views/livewire/committee/edit-committee.blade.php
+++ resources/views/livewire/committee/edit-committee.blade.php
@@ -1,4 +1,4 @@
-<x-livewire-form class="space-y-8">
+<x-livewire-form class="w-full space-y-8">
     <div>
         <flux:heading size="xl" class="mb-4">{{  __('committees.edit_headline', ['name' => $description]) }}</flux:heading>
         <flux:text class="text-base">{{  __('committees.edit_explanation', ['name' => $description]) }}</flux:text>
