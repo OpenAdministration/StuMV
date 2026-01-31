1cac472e865b64aa4d6c0ba114ff69367ecd3fce
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-31 22:21:00 +0100

    Delete committee modal text

--- resources/views/livewire/committee/committee-tree-item.blade.php
+++ resources/views/livewire/committee/committee-tree-item.blade.php
@@ -99,9 +99,9 @@
     <flux:modal name="delete-committee-{{ $committee->getFirstAttribute('ou') }}" class="md:w-96">
         <div class="space-y-6">
             <div>
-                <flux:heading size="lg">{{ __('committees.delete_title', ['name' => $deleteCommitteeName]) }}</flux:heading>
-                <flux:text class="mt-2">{{ __('committees.delete_warning', ['name' => $deleteCommitteeName]) }}</flux:text>
-                <flux:text class="mt-2">{{ __('committees.delete.confirm') }}<strong>{{ $deleteCommitteeOu }}</strong></flux:text>
+                <flux:heading size="lg">{{ __('committees.delete_title', ['name' => $committee->getFirstAttribute('description')]) }}</flux:heading>
+                <flux:text class="mt-2">{{ __('committees.delete_warning', ['name' => $committee->getFirstAttribute('description')]) }}</flux:text>
+                <flux:text class="mt-2">{{ __('committees.delete.confirm') }}<strong>{{ {{ $committee->getFirstAttribute('ou') }} }}</strong></flux:text>
             </div>
             <div class="flex">
                 <flux:spacer />
