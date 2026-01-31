73fd3902e2034e0e9c112aa4918d3729b9e2f4a1
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-01-31 05:13:35 +0100

    Try to show sync button only for superadmins

--- resources/views/livewire/sync.blade.php
+++ resources/views/livewire/sync.blade.php
@@ -1,18 +1,17 @@
-<flux:dropdown>
-    <flux:button
-        variant="ghost"
-        icon="refresh-ccw"
-    />
-    <flux:menu>
-        <flux:menu.item
-            icon="folder-tree"
-            wire:click="sync"
-            title="{{ __('sync.ldap_title') }}"
-            @cannot('superadmin', \App\Models\User::class)
-                disabled
-            @endcannot
-        >
-            LDAP
-        </flux:menu.item>
-    </flux:menu>
-</flux:dropdown>
+@can('superadmin', \App\Models\User::class)
+    <flux:dropdown>
+        <flux:button
+            variant="ghost"
+            icon="refresh-ccw"
+        />
+        <flux:menu>
+            <flux:menu.item
+                icon="folder-tree"
+                wire:click="sync"
+                title="{{ __('sync.ldap_title') }}"
+            >
+                LDAP
+            </flux:menu.item>
+        </flux:menu>
+    </flux:dropdown>
+@endcan
