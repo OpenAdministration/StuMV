bd09578d5cc98c3ba92d385a7e975d898e930fde
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-04 00:32:48 +0100

    max-width

--- resources/views/livewire/committee/edit-role.blade.php
+++ resources/views/livewire/committee/edit-role.blade.php
@@ -1,4 +1,4 @@
-<x-livewire-form class="w-full space-y-8">
+<x-livewire-form class="max-w-6xl mx-auto w-full space-y-8">
     <div>
         <flux:heading size="xl">{{  __('roles.edit_heading', ['name' => $cn]) }}</flux:heading>
     </div>
