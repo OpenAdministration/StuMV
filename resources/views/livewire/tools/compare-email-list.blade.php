d6cdd78eeb9a6036e3c9d729974df75c44da5cda
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-04 02:06:08 +0100

    Styling

--- resources/views/livewire/tools/compare-email-list.blade.php
+++ resources/views/livewire/tools/compare-email-list.blade.php
@@ -15,7 +15,7 @@
         <div>
             <flux:fieldset>
                 <flux:legend>{{ __('tools.matches') }}</flux:legend>
-                <div class="flex flex-col divide-y divide-zinc-200 dark:divide-zinc-700">
+                <div class="-mt-3 flex flex-col divide-y divide-zinc-200 dark:divide-zinc-700">
                     @foreach($matches as $user)
                         <div class="py-3">
                             <flux:link
