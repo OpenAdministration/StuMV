799e286b2de9f7a2bd08a6b577ceb26cc857b4fe
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-04 02:22:15 +0100

    German tools translations

---
+++ lang/de/tools.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'checkForMatches' => 'Prüfe auf Treffer',
+    'compareEmailList_headline' => 'E-Mail-Listen-Abgleich',
+    'compareEmailList_explanation' => 'Füge in das Textfeld eine Liste von E-Mail-Adressen ein (getrennt durch Zeilenumbrüche) und überprüfe, ob Benutzer:innen mit dieser E-Mail-Adresse existieren.',
+    'emailAddresses' => 'E-Mail-Adressen',
+    'matches' => 'Treffer',
+    'tools' => 'Hilfsmittel',
+];
