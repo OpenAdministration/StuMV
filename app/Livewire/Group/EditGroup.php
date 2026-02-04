6832e1babb1e2179a1aa220203d1655fb1b0c9f3
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-04 03:26:52 +0100

    Edit groups

--- app/Livewire/Group/EditGroup.php
+++ app/Livewire/Group/EditGroup.php
@@ -2,17 +2,53 @@
 
 namespace App\Livewire\Group;
 
+use Flux\Flux;
+use LdapRecord\Models\OpenLDAP\Group;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class EditGroup extends Component
 {
-    public function mount($cn) :void
-    {
+    #[Locked]
+    public string $uid;
 
+    #[Locked]
+    public string $dn;
+
+    #[Locked]
+    public string $cn;
+
+    #[Rule('required|min:6')]
+    public string $name;
+
+    public function mount(Community $uid, $cn)
+    {
+        $this->uid = $uid->getFirstAttribute('ou');
+        $this->cn = $cn;
+        $this->dn = Group::dnFrom($this->realm_uid, $cn);
+        $group = Group::findOrFail($this->dn);
+        $this->name = $group->getFirstAttribute('description');
     }
 
     public function render()
     {
-        return view('livewire.group.edit-group')->title(__('groups.roles_add_title'));
+        return view('livewire.group.edit-group');
+    }
+
+    public function save()
+    {
+        $this->validate();
+        try {
+            $group = Group::findOrFail($this->dn);
+            $group->save([
+                'description' => $this->name,
+            ]);
+
+            Flux::toast(variant: 'success', text: __('groups.edit_success'));
+            return redirect()->route('realms.groups.roles', ['uid' => $this->uid, 'cn' => $this->cn]);
+        } catch (LdapRecordException $exception) {
+            $this->addError('cn', $exception->getMessage());
+            return false;
+        }
     }
 }
--- resources/views/livewire/group/edit-group.blade.php
+++ resources/views/livewire/group/edit-group.blade.php
@@ -1,3 +1,15 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-</div>
+<x-livewire-form class="max-w-6xl mx-auto w-full space-y-8">
+    <div>
+        <flux:heading size="xl">{{  __('groups.edit', ['name' => $cn]) }}</flux:heading>
+    </div>
+
+    <flux:field>
+        <flux:label>{{ __('Short Groupname') }}</flux:label>
+        <flux:input wire:model="cn" />
+    </flux:field>
+    
+    <flux:field>
+        <flux:label>{{ __('Full Groupname') }}</flux:label>
+        <flux:input wire:model="name" />
+    </flux:field>
+</x-livewire-form>
