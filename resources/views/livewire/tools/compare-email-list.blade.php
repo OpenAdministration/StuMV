1145bb35c34dfbf8dc1f3b5f07ae4b82b68665d0
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-04 02:00:27 +0100

    Display user data in results

--- resources/views/livewire/tools/compare-email-list.blade.php
+++ resources/views/livewire/tools/compare-email-list.blade.php
@@ -20,9 +20,9 @@
                     <div class="w-full px-4 py-2 border-b border-zinc-200 dark:border-zinc-700">
                         <flux:link
                             wire:navigate
-                            href="{{ route('profile', ['username' => $user->uid]) }}"
+                            href="{{ route('profile', ['username' => $user['uid']]) }}"
                         >
-                            {{ $user->cn }}
+                            {{ $user['cn'] }}
                         </flux:link>
                     </div>
                 @endforeach
