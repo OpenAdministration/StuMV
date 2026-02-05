4bea83a68065f22ad1c24d033b064c5f058f061b
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-05 18:32:05 +0100

    Fix problems

--- app/Livewire/Tools/UsersNotInUniLdap.php
+++ app/Livewire/Tools/UsersNotInUniLdap.php
@@ -57,13 +57,8 @@ class UsersNotInUniLdap extends Component
                     $filter = "(|(mail=$member->email))";
                     $result = ldap_search($ds, $unildap->members_base, $filter);
                     $info = ldap_get_entries($ds, $result);
-                    if ($info['count'] !== 1) {
-                        $entry = $info[0];
-                        if (is_array($entry) && isset($entry['mail']) && !is_array($entry['mail'])) {
-                            $entry['mail'] = [$entry['mail']];
-                        }
-
-                        $user = User::findByEmail($entry['mail']);
+                    if ($info['count'] === 0) {
+                        $user = User::findByEmail($member->email);
                         if ($user !== null) {
                             $this->results[] = [
                                 'uid' => $user->getFirstAttribute('uid'),
