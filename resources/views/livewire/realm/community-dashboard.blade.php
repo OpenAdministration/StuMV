3f6a0828293d6d39e5cb522ff0d4db8e5525410c
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-02-08 05:59:30 +0100

    Hide items the user does not have access to because disable does not work

--- resources/views/livewire/realm/community-dashboard.blade.php
+++ resources/views/livewire/realm/community-dashboard.blade.php
@@ -74,50 +74,53 @@
                 <flux:text class="mt-2">{{ __('realms.dashboard.admin_explanation', ['name' => $name]) }}</flux:text>
             </flux:card>
         </a>
-        <a
-            wire:navigate
-            href="{{ route('realms.edit', $uid) }}"
-            aria-label="{{ __('realms.dashboard.realms_edit_headline', ['name' => $name]) }}"
-            disabled="{{ auth()->user()->cannot('edit', $community) }}"
-            class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
-        >
-            <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
-                <flux:icon.landmark class="size-5" />
-            </div>
-            <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
-                <flux:heading size="lg">{{ __('realms.dashboard.realms_edit_headline', ['name' => $name]) }}</flux:heading>
-                <flux:text class="mt-2">{{ __('realms.dashboard.realms_edit_explanation', ['name' => $name]) }}</flux:text>
-            </flux:card>
-        </a>
-        <a
-            wire:navigate
-            href="{{ route('realms.groups', $uid) }}"
-            aria-label="{{ __('realms.dashboard.groups_headline', ['name' => $name]) }}"
-            disabled="{{ auth()->user()->cannot('viewAny', [\App\Ldap\Group::class, $community]) }}"
-            class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
-        >
-            <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
-                <flux:icon.key-round class="size-5" />
-            </div>
-            <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
-                <flux:heading size="lg">{{ __('realms.dashboard.groups_headline', ['name' => $name]) }}</flux:heading>
-                <flux:text class="mt-2">{{ __('realms.dashboard.groups_explanation', ['name' => $name]) }}</flux:text>
-            </flux:card>
-        </a>
-        <a
-            wire:navigate
-            href="{{ route('realms.domains', $uid) }}"
-            aria-label="{{ __('realms.dashboard.domains_headline', ['name' => $name]) }}"
-            disabled="{{ auth()->user()->cannot('viewAny', [\App\Ldap\Domain::class, $community]) }}"
-            class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
-        >
-            <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
-                <flux:icon.globe class="size-5" />
-            </div>
-            <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
-                <flux:heading size="lg">{{ __('realms.dashboard.domains_headline', ['name' => $name]) }}</flux:heading>
-                <flux:text class="mt-2">{{ __('realms.dashboard.domains_explanation', ['name' => $name]) }}</flux:text>
-            </flux:card>
-        </a>
+        @can('edit', $community)
+            <a
+                wire:navigate
+                href="{{ route('realms.edit', $uid) }}"
+                aria-label="{{ __('realms.dashboard.realms_edit_headline', ['name' => $name]) }}"
+                class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
+            >
+                <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
+                    <flux:icon.landmark class="size-5" />
+                </div>
+                <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
+                    <flux:heading size="lg">{{ __('realms.dashboard.realms_edit_headline', ['name' => $name]) }}</flux:heading>
+                    <flux:text class="mt-2">{{ __('realms.dashboard.realms_edit_explanation', ['name' => $name]) }}</flux:text>
+                </flux:card>
+            </a>
+        @endcan
+        @can('viewAny', [\App\Ldap\Group::class, $community])
+            <a
+                wire:navigate
+                href="{{ route('realms.groups', $uid) }}"
+                aria-label="{{ __('realms.dashboard.groups_headline', ['name' => $name]) }}"
+                class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
+            >
+                <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
+                    <flux:icon.key-round class="size-5" />
+                </div>
+                <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
+                    <flux:heading size="lg">{{ __('realms.dashboard.groups_headline', ['name' => $name]) }}</flux:heading>
+                    <flux:text class="mt-2">{{ __('realms.dashboard.groups_explanation', ['name' => $name]) }}</flux:text>
+                </flux:card>
+            </a>
+        @endcan
+        @can('viewAny', [\App\Ldap\Group::class, $community])
+            <a
+                wire:navigate
+                href="{{ route('realms.domains', $uid) }}"
+                aria-label="{{ __('realms.dashboard.domains_headline', ['name' => $name]) }}"
+                class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
+            >
+                <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
+                    <flux:icon.globe class="size-5" />
+                </div>
+                <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
+                    <flux:heading size="lg">{{ __('realms.dashboard.domains_headline', ['name' => $name]) }}</flux:heading>
+                    <flux:text class="mt-2">{{ __('realms.dashboard.domains_explanation', ['name' => $name]) }}</flux:text>
+                </flux:card>
+            </a>
+        @endcan
     </div>
 </div>
