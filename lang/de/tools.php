9b28b639162a7e46ed2f23a98c688bd48aa9f6a3
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-06-09 20:20:54 +0200

    Add translation

--- lang/de/tools.php
+++ lang/de/tools.php
@@ -5,6 +5,7 @@ return [
     'compareEmailList_headline' => 'E-Mail-Listen-Abgleich',
     'compareEmailList_explanation' => 'Füge in das Textfeld eine Liste von E-Mail-Adressen ein (getrennt durch Zeilenumbrüche) und überprüfe, ob Benutzer:innen mit dieser E-Mail-Adresse existieren.',
     'createUser' => 'Person anlegen',
+    'delete' => 'Löschen',
     'emailAddress' => 'E-Mail-Adresse',
     'emailAddresses' => 'E-Mail-Adressen',
     'firstname' => 'Vorname',
