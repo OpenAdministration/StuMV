cb5ba8fa1a97f94b75b4d6edf1d71f52ec333287
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-06-20 18:22:32 +0200

    Add unused roles to tools dashboard

--- resources/views/livewire/tools/tools-dashboard.blade.php
+++ resources/views/livewire/tools/tools-dashboard.blade.php
@@ -43,5 +43,18 @@
                 <flux:text class="mt-2">{{ __('tools.usersNotInUniLdap_explanation') }}</flux:text>
             </flux:card>
         </a>
+        <a
+            wire:navigate
+            href="{{ route('tools.unused-roles', $uid) }}"
+            class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
+        >
+            <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
+                <flux:icon.list-x class="size-5" />
+            </div>
+            <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
+                <flux:heading size="lg">{{ __('tools.unusedRoles_headline') }}</flux:heading>
+                <flux:text class="mt-2">{{ __('tools.unusedRoles_explanation') }}</flux:text>
+            </flux:card>
+        </a>
     </div>
 </div>
