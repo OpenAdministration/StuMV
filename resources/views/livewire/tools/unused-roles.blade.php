e10de62996413132b76d767cc84dce3389c99221
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-06-20 16:35:04 +0200

    Use flux ui skeleton

--- resources/views/livewire/tools/unused-roles.blade.php
+++ resources/views/livewire/tools/unused-roles.blade.php
@@ -10,50 +10,68 @@
                 <flux:tab name="committees">{{ __('tools.committees') }}</flux:tab>
             </flux:tabs>
             <flux:tab.panel name="roles">
-                <flux:table>
-                    <flux:table.columns>
-                        <flux:table.column>{{ __('tools.role') }}</flux:table.column>
-                        <flux:table.column>{{ __('tools.committee') }}</flux:table.column>
-                    </flux:table.columns>
-                    <flux:table.rows>
-                        @foreach($unusedRoles as $role)
-                            <flux:table.row>
-                                <flux:table.cell>
-                                    {{ $role->getFirstAttribute('description') }}
-                                </flux:table.cell>
-                                <flux:table.cell>
-                                    <flux:link
-                                        wire:navigate
-                                        href="{{ route('committees.roles', ['uid' => $realm_uid, 'ou' => $role->committee()->getFirstAttribute('ou')]) }}"
-                                    >
-                                        {{ $role->committee()->getFirstAttribute('description') }}
-                                    </flux:link>
-                                </flux:table.cell>
-                            </flux:table.row>
-                        @endforeach
-                    </flux:table.rows>
-                </flux:table>
+                <div wire:loading.remove>
+                    <flux:table>
+                        <flux:table.columns>
+                            <flux:table.column>{{ __('tools.role') }}</flux:table.column>
+                            <flux:table.column>{{ __('tools.committee') }}</flux:table.column>
+                        </flux:table.columns>
+                        <flux:table.rows>
+                            @foreach($unusedRoles as $role)
+                                <flux:table.row>
+                                    <flux:table.cell>
+                                        {{ $role->getFirstAttribute('description') }}
+                                    </flux:table.cell>
+                                    <flux:table.cell>
+                                        <flux:link
+                                            wire:navigate
+                                            href="{{ route('committees.roles', ['uid' => $realm_uid, 'ou' => $role->committee()->getFirstAttribute('ou')]) }}"
+                                        >
+                                            {{ $role->committee()->getFirstAttribute('description') }}
+                                        </flux:link>
+                                    </flux:table.cell>
+                                </flux:table.row>
+                            @endforeach
+                        </flux:table.rows>
+                    </flux:table>
+                </div>
+                <div wire:loading.delay class="space-y-3">
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                </div>
             </flux:tab.panel>
             <flux:tab.panel name="committees">
-                <flux:table>
-                    <flux:table.columns>
-                        <flux:table.column>{{ __('tools.committee') }}</flux:table.column>
-                    </flux:table.columns>
-                    <flux:table.rows>
-                        @foreach($unusedCommittees as $committee)
-                            <flux:table.row>
-                                <flux:table.cell>
-                                    <flux:link
-                                        wire:navigate
-                                        href="{{ route('committees.roles', ['uid' => $realm_uid, 'ou' => $committee->getFirstAttribute('ou')]) }}"
-                                    >
-                                        {{ $committee->getFirstAttribute('description') }}
-                                    </flux:link>
-                                </flux:table.cell>
-                            </flux:table.row>
-                        @endforeach
-                    </flux:table.rows>
-                </flux:table>
+                <div wire:loading.remove>
+                    <flux:table>
+                        <flux:table.columns>
+                            <flux:table.column>{{ __('tools.committee') }}</flux:table.column>
+                        </flux:table.columns>
+                        <flux:table.rows>
+                            @foreach($unusedCommittees as $committee)
+                                <flux:table.row>
+                                    <flux:table.cell>
+                                        <flux:link
+                                            wire:navigate
+                                            href="{{ route('committees.roles', ['uid' => $realm_uid, 'ou' => $committee->getFirstAttribute('ou')]) }}"
+                                        >
+                                            {{ $committee->getFirstAttribute('description') }}
+                                        </flux:link>
+                                    </flux:table.cell>
+                                </flux:table.row>
+                            @endforeach
+                        </flux:table.rows>
+                    </flux:table>
+                </div>
+                <div wire:loading.delay class="space-y-3">
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                    <flux:skeleton class="h-10 w-full" />
+                </div>
             </flux:tab.panel>
         </flux:tab.group>
     </div>
