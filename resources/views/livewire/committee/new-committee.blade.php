266c787e696839f006cae8a3d11595a408df0775
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-06-26 20:52:47 +0200

    New committee view

--- resources/views/livewire/committee/new-committee.blade.php
+++ resources/views/livewire/committee/new-committee.blade.php
@@ -1,6 +1,6 @@
 <x-livewire-form class="max-w-6xl mx-auto w-full">
     <flux:heading size="xl" class="mb-6">{{ __('New Committee') }}</flux:heading>
-    <flux:field class="mb-6">
+    <flux:field class="flex flex-col mb-6">
         <flux:label>{{ __('Parent Committee') }}</flux:label>
         <flux:select
             variant="listbox"
@@ -14,24 +14,24 @@
             @endforeach
         </flux:select>
     </flux:field>
-    <flux:field class="mb-6">
+    <flux:field class="flex flex-col mb-6">
         <flux:label>{{ __('Short Committee Name') }}</flux:label>
         <flux:description>{{ __('committees.new_hint_shortname') }}</flux:description>
         <flux:input type="text" wire:model.live="ou" required />
         <flux:error name="ou" />
     </flux:field>
-    <flux:field>
+    <flux:field class="flex flex-col mb-6">
         <flux:label>{{ __('Full Committee Name') }}</flux:label>
         <flux:description>{{ __('committees.new_hint_longname') }}</flux:description>
         <flux:input type="text" wire:model.live="description" required />
         <flux:error name="description" />
     </flux:field>
-    <flux:field>
+    <flux:field class="flex flex-col">
         <flux:label>{{ __('committees.add_roles') }}</flux:label>
-        <flux:description>{{ __('committees.new_hint_longname') }}</flux:description>
-        <flux:pillbox wire:model="roles">
+        <flux:description>{{ __('committees.new_hint_roles') }}</flux:description>
+        <flux:pillbox wire:model="roles" multiple>
             @foreach($defaultRoles as $key => $r)
-                <flux:pillbox.option value="{{ $key }}">{{ $r->description }}</flux:pillbox.option>
+                <flux:pillbox.option value="{{ $key }}">{{ $r['description'] }}</flux:pillbox.option>
             @endforeach
         </flux:pillbox>
         <flux:error name="roles" />
