151e8936b744fdde5bdfce95b12a21bd4453f6b6
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-01 00:45:58 +0200

    Title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" href="{{ asset('img/logo.svg') }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ $title ? $title . ' | ' . config('app.name') : config('app.name') }}</title>
 
         @livewireStyles
         @fluxAppearance
