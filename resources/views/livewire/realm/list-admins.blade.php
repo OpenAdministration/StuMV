03b9c9652fdc7c4ff61d735bf891dca8b6be0f83
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-03 16:29:44 +0200

    Add 1px div

--- resources/views/livewire/realm/list-admins.blade.php
+++ resources/views/livewire/realm/list-admins.blade.php
@@ -92,6 +92,8 @@
         </flux:table.rows>
     </flux:table>
 
+    <div class="block h-[1px]"></div>
+
     <form wire:submit="deleteCommit">
         <flux:modal name="delete">
             <div class="space-y-6">
