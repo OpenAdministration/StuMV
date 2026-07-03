e4fac86b2cc9d8c2ffb281dfdb243155e981c6aa
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-03 17:34:49 +0200

    Update translations

--- lang/de/roles.php
+++ lang/de/roles.php
@@ -40,5 +40,6 @@ return [
     'edit_success' => 'Rolle erfolgreich bearbeitet',
     'membership-edit_headline' => 'Rollenzuordnung anpassen',
     'membership-edit_explanation' => 'Hier kannst du die Zuordnung des:der Nutzers:Nutzerin bearbeiten. Das Start- und Enddatum begrenzt die Rollenzuordnung. Liegen die Daten in der Zukunft, wird die Zuordnung dann erst begonnen oder beendet. Beschlussdatum und Kommentar sind optionale Informationen, die lediglich der Beschreibung dienen.',
-    'there_are_inactive_roles' => 'In diesem Gremium gibt es Rollen ohne aktive Mitgliedschaften. Um diese Rollen angezeigt zu bekommen, lege oben den Schalter um.'
+    'there_are_inactive_roles' => 'In diesem Gremium gibt es Rollen ohne aktive Mitgliedschaften. Um diese Rollen angezeigt zu bekommen, lege oben den Schalter um.',
+    'edit_member_success' => 'Die Änderungen an der Mitgliedschaft wurden erfolgreich gespeichert.',
 ];
