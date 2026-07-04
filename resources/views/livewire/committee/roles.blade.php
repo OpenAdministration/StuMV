28d3467eca8b1820e5e5a20ced4f0cfec0811288
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-04 02:16:55 +0200

    Remove if

--- resources/views/livewire/committee/roles.blade.php
+++ resources/views/livewire/committee/roles.blade.php
@@ -31,87 +31,83 @@
         $hasHiddenRolesWithMembers = false;
         $committeesShown = 0;
     @endphp
-    @if($committeesShown > 0)
-        <div class="grid lg:grid-cols-2 gap-6">
-    @endif
-    @forelse($roles as $role)
-        @php
-            $hasMembers = $this->getHasMembers($role);
-            if (!$hasMembers && $this->showOnlyActive) {
-                $hasHiddenRolesWithMembers = true;
-            }
-            if ($hasMembers) {
-                $committeesShown = $committeesShown + 1;
-            }
-        @endphp
-        @if($this->showOnlyActive && $this->getHasMembers($role) || !$this->showOnlyActive)
-            <flux:card>
-                <div class="flex gap-4 items-center">
-                    <div class="flex-1">
-                        <flux:link
-                            wire:navigate
-                            href="{{ route('committees.roles.members', ['uid' => $uid, 'ou' => $ou, 'cn' => $role->getFirstAttribute('cn')]) }}"
-                        >
-                            {{ $role->getFirstAttribute('description') }}
-                        </flux:link>
+    <div class="grid lg:grid-cols-2 gap-6">
+        @forelse($roles as $role)
+            @php
+                $hasMembers = $this->getHasMembers($role);
+                if (!$hasMembers && $this->showOnlyActive) {
+                    $hasHiddenRolesWithMembers = true;
+                }
+                if ($hasMembers) {
+                    $committeesShown = $committeesShown + 1;
+                }
+            @endphp
+            @if($this->showOnlyActive && $this->getHasMembers($role) || !$this->showOnlyActive)
+                <flux:card>
+                    <div class="flex gap-4 items-center">
+                        <div class="flex-1">
+                            <flux:link
+                                wire:navigate
+                                href="{{ route('committees.roles.members', ['uid' => $uid, 'ou' => $ou, 'cn' => $role->getFirstAttribute('cn')]) }}"
+                            >
+                                {{ $role->getFirstAttribute('description') }}
+                            </flux:link>
+                        </div>
+                        <div>
+                            <flux:dropdown>
+                                <flux:button size="sm" icon="ellipsis-vertical" />
+                                <flux:menu>
+                                    <flux:menu.item
+                                        icon="users"
+                                        wire:navigate
+                                        :href="route('committees.roles.members', ['uid' => $uid, 'ou' => $ou, 'cn' => $role->getFirstAttribute('cn')])"
+                                    >
+                                        {{ __('roles.link_members') }}
+                                    </flux:menu.item>
+                                    <flux:menu.item
+                                        icon="pencil"
+                                        wire:navigate
+                                        :href="route('committees.roles.edit', ['uid' => $uid, 'ou' => $ou, 'cn' => $role->getFirstAttribute('cn')])"
+                                        :disabled="auth()->user()->cannot('edit', [$role, $committee, $community])"
+                                    >
+                                        {{ __('roles.link_edit') }}
+                                    </flux:menu.item>
+                                    <flux:menu.item
+                                        variant="danger"
+                                        icon="trash-2"
+                                        :disabled="auth()->user()->cannot('delete', [$role, $committee, $community])"
+                                        wire:click="deletePrepare('{{ $role->getFirstAttribute('cn') }}')">
+                                        {{ __('Delete') }}
+                                    </flux:menu.item>
+                                </flux:menu>
+                            </flux:dropdown>
+                        </div>
                     </div>
-                    <div>
-                        <flux:dropdown>
-                            <flux:button size="sm" icon="ellipsis-vertical" />
-                            <flux:menu>
-                                <flux:menu.item
-                                    icon="users"
-                                    wire:navigate
-                                    :href="route('committees.roles.members', ['uid' => $uid, 'ou' => $ou, 'cn' => $role->getFirstAttribute('cn')])"
-                                >
-                                    {{ __('roles.link_members') }}
-                                </flux:menu.item>
-                                <flux:menu.item
-                                    icon="pencil"
-                                    wire:navigate
-                                    :href="route('committees.roles.edit', ['uid' => $uid, 'ou' => $ou, 'cn' => $role->getFirstAttribute('cn')])"
-                                    :disabled="auth()->user()->cannot('edit', [$role, $committee, $community])"
-                                >
-                                    {{ __('roles.link_edit') }}
-                                </flux:menu.item>
-                                <flux:menu.item
-                                    variant="danger"
-                                    icon="trash-2"
-                                    :disabled="auth()->user()->cannot('delete', [$role, $committee, $community])"
-                                    wire:click="deletePrepare('{{ $role->getFirstAttribute('cn') }}')">
-                                    {{ __('Delete') }}
-                                </flux:menu.item>
-                            </flux:menu>
-                        </flux:dropdown>
-                    </div>
-                </div>
-                @if($this->getHasMembers($role))
-                    <div class="flex flex-wrap gap-2 mt-6">
-                        @foreach($this->getMembers($role) as $member)
-                            @php
-                                $jpegPhoto = $member->getFirstAttribute('jpegPhoto');
-                                if ($jpegPhoto) {
-                                    $jpegPhoto = 'data:image/jpeg;base64,' . $jpegPhoto;
-                                }
-                            @endphp
-                            <flux:tooltip content="{{ $member->getFirstAttribute('cn') }}">
-                                <flux:avatar
-                                    size="lg"
-                                    src="{{ $jpegPhoto }}"
-                                    name="{{ $member->getFirstAttribute('cn') }}"
-                                />
-                            </flux:tooltip>
-                        @endforeach
-                    </div>
-                @endif
-            </flux:card>
-        @endif
-    @empty
-        <flux:callout variant="warning" icon="info" heading="{{ __('roles.no_roles_found') }}" class="col-span-full" />
-    @endforelse
-    @if($committeesShown > 0)
-        </div>
-    @endif
+                    @if($this->getHasMembers($role))
+                        <div class="flex flex-wrap gap-2 mt-6">
+                            @foreach($this->getMembers($role) as $member)
+                                @php
+                                    $jpegPhoto = $member->getFirstAttribute('jpegPhoto');
+                                    if ($jpegPhoto) {
+                                        $jpegPhoto = 'data:image/jpeg;base64,' . $jpegPhoto;
+                                    }
+                                @endphp
+                                <flux:tooltip content="{{ $member->getFirstAttribute('cn') }}">
+                                    <flux:avatar
+                                        size="lg"
+                                        src="{{ $jpegPhoto }}"
+                                        name="{{ $member->getFirstAttribute('cn') }}"
+                                    />
+                                </flux:tooltip>
+                            @endforeach
+                        </div>
+                    @endif
+                </flux:card>
+            @endif
+        @empty
+            <flux:callout variant="warning" icon="info" heading="{{ __('roles.no_roles_found') }}" class="col-span-full" />
+        @endforelse
+    </div>
     @if($hasHiddenRolesWithMembers && $committeesShown < 1)
         <flux:callout variant="warning" icon="info" heading="{{ __('roles.there_are_inactive_roles') }}" />
     @endif
