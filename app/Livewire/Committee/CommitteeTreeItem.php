579637ad34940771d1c76ff18279a5e3de49e0bc
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-07 13:30:13 +0200

    Improve deleting committee

--- app/Livewire/Committee/CommitteeTreeItem.php
+++ app/Livewire/Committee/CommitteeTreeItem.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Committee;
 
 use App\Ldap\Committee;
 use App\Ldap\Community;
+use App\Models\GroupMembership;
+use App\Models\RoleMembership;
 use Flux\Flux;
 use Livewire\Component;
 
@@ -37,6 +39,8 @@ class CommitteeTreeItem extends Component
 
     public function deleteCommittee(string $dn, string $cn)
     {
+        $this->authorize('delete', [$c, $community]);
+
         $community = Community::findByUid($this->realm_uid);
         $c = Committee::findOrFail($dn);
 
@@ -45,7 +49,30 @@ class CommitteeTreeItem extends Component
             return;
         }
 
-        $this->authorize('delete', [$c, $community]);
+        // Delete role memberships and role group relashionships for this committee and its descendants
+        $dn = $c->getDn();
+        $roles = $c->roles()->get();
+        foreach ($roles as $role) {
+            RoleMembership::where('role_cn', $role->getFirstAttribute('cn'))
+                ->where('committee_dn', $dn)
+                ->delete();
+            GroupMembership::where('role_dn', $role->getDn())
+                ->delete();
+        }
+
+        $descendants = $c->descendants()->get();
+        foreach ($descendants as $descendant) {
+            $roles = $descendant->roles()->get();
+            foreach ($roles as $role) {
+                RoleMembership::where('role_cn', $role->getFirstAttribute('cn'))
+                    ->where('committee_dn', $descendant->getDn())
+                    ->delete();
+                GroupMembership::where('role_dn', $role->getDn())
+                    ->delete();
+            }
+        }
+
+        // Delete the committee and its descendants recursively
         $c->delete(recursive: true);
 
         Flux::modal('delete-committee-' . $cn)->close();
