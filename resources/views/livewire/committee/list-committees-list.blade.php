890f8d5f6f5fbe5a3a8119da6823ff4ab0a31c69
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-08 03:57:31 +0200

    Div around dropdown

--- resources/views/livewire/committee/list-committees-list.blade.php
+++ resources/views/livewire/committee/list-committees-list.blade.php
@@ -32,34 +32,36 @@
                                 </flux:link>
                             </flux:table.cell>
                             <flux:table.cell>
-                                <flux:dropdown>
-                                    <flux:button size="sm" icon="ellipsis-vertical" title="{{ __('common.options') }}" />
-                                    <flux:menu>
-                                        <flux:menu.item
-                                            icon="users"
-                                            wire:navigate
-                                            href="{{ route('committees.roles', ['uid' => $realm_uid, 'ou' => $committee->getFirstAttribute('ou')]) }}"
-                                            class="md:hidden"
-                                        >
-                                            {{ __('committees.link_roles') }}
-                                        </flux:menu.item>
-                                        <flux:menu.item
-                                            icon="pencil"
-                                            href="{{ route('committees.edit', ['uid' => $realm_uid, 'ou' => $committee->getFirstAttribute('ou')]) }}"
-                                            :disabled="auth()->user()->cannot('edit', [$committee, $community])"
-                                        >
-                                            {{ __('committees.link_edit') }}
-                                        </flux:menu.item>
-                                        <flux:menu.item
-                                            variant="danger"
-                                            icon="trash-2"
-                                            x-on:click="$flux.modal('delete-committee-{{ $committee->getFirstAttribute('ou') }}').show()"
-                                            :disabled="auth()->user()->cannot('edit', [$committee, $community])"
-                                        >
-                                            {{ __('Delete') }}
-                                        </flux:menu.item>
-                                    </flux:menu>
-                                </flux:dropdown>
+                                <div class="flex justify-end items-center gap-2">
+                                    <flux:dropdown>
+                                        <flux:button size="sm" icon="ellipsis-vertical" title="{{ __('common.options') }}" />
+                                        <flux:menu>
+                                            <flux:menu.item
+                                                icon="users"
+                                                wire:navigate
+                                                href="{{ route('committees.roles', ['uid' => $realm_uid, 'ou' => $committee->getFirstAttribute('ou')]) }}"
+                                                class="md:hidden"
+                                            >
+                                                {{ __('committees.link_roles') }}
+                                            </flux:menu.item>
+                                            <flux:menu.item
+                                                icon="pencil"
+                                                href="{{ route('committees.edit', ['uid' => $realm_uid, 'ou' => $committee->getFirstAttribute('ou')]) }}"
+                                                :disabled="auth()->user()->cannot('edit', [$committee, $community])"
+                                            >
+                                                {{ __('committees.link_edit') }}
+                                            </flux:menu.item>
+                                            <flux:menu.item
+                                                variant="danger"
+                                                icon="trash-2"
+                                                x-on:click="$flux.modal('delete-committee-{{ $committee->getFirstAttribute('ou') }}').show()"
+                                                :disabled="auth()->user()->cannot('edit', [$committee, $community])"
+                                            >
+                                                {{ __('Delete') }}
+                                            </flux:menu.item>
+                                        </flux:menu>
+                                    </flux:dropdown>
+                                </div>
                             </flux:table.cell>
                         </flux:table.row>
                     @empty
