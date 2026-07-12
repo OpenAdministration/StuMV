5c27fc1f69feec94441ee589999df5710c910ba0
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-12 19:18:12 +0200

    Do not use form component to remove cancel button

--- resources/views/livewire/change-password.blade.php
+++ resources/views/livewire/change-password.blade.php
@@ -6,8 +6,8 @@
     <x-navbar-profile :username="$currentUsername" />
 
     <div class="flex-1 p-6 sm:p-8 overflow-y-auto">
-        <div class="max-w-6xl mx-auto space-y-6">
-            <x-livewire-form class="space-y-6">
+        <div class="max-w-6xl mx-auto">
+            <form wire:submit="save" class="space-y-6">
                 <flux:field>
                     <flux:label class="block">{{ __('Password') }}</flux:label>
                     <flux:description class="block">{{ __('user.help.password') }}</flux:description>
@@ -20,7 +20,11 @@
                     <flux:input type="password" wire:model="password_confirmation" />
                     <flux:error name="password_confirmation" />
                 </flux:field>
-            </x-livewire-form>
+
+                <div class="flex items-center justify-end gap-x-3">
+                    <flux:button variant="primary" icon="save" type="submit">{{ __('Save') }}</flux:button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
