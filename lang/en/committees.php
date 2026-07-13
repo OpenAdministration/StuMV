ffb409548a28ba5ba7f2ec701faf03ba9ed03b3f
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-13 17:14:15 +0200

    Update translations

--- lang/en/committees.php
+++ lang/en/committees.php
@@ -35,7 +35,7 @@ return [
     'showOnlyActiveRoles' => 'Show only roles with active memberships',
     'link_moderators' => 'Moderators',
     'mods_heading' => 'Moderators of :name',
-    'mods_explanation' => 'Moderators of a committee can manage it and its sub-committees exactly like general moderators - creating, editing and deleting committees and roles, and managing role memberships - but only within this committee and its descendants.',
+    'mods_explanation' => 'Moderators of a committee can, like general moderators, create, edit and delete committees and roles, and manage role memberships. However, they are limited to this committee and its subordinate committees.',
     'no_mods_found' => 'No moderators found!',
     'new_mod_headline' => 'Add new moderator of :name',
     'new_mod_explanation' => 'Select a user who should become a moderator of this committee and its sub-committees.',
