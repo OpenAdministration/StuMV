261aae4ec9585b582b83acbe39f7db819ef7fe26
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-15 15:15:40 +0200

    Try to fix breadcrumbs bugs

--- resources/views/vendor/breadcrumbs/title.blade.php
+++ resources/views/vendor/breadcrumbs/title.blade.php
@@ -1,1 +1,5 @@
-{{ $breadcrumb->title }}
+{{-- Kept on one line (not truncated) so the responsive collapse in
+     tailwind.blade.php can measure natural widths and detect overflow -
+     without this, text would just wrap onto multiple lines instead of
+     overflowing, and nothing would ever need to collapse. --}}
+<span class="whitespace-nowrap">{{ $breadcrumb->title }}</span>
