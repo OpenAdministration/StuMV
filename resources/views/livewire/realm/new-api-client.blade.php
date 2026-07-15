9a732979376738cfb9a998a21624ee61cb89e56d
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-16 01:32:12 +0200

    Add wrapping div

--- resources/views/livewire/realm/new-api-client.blade.php
+++ resources/views/livewire/realm/new-api-client.blade.php
@@ -24,28 +24,30 @@
         </div>
     </div>
 @else
-    <x-livewire-form class="space-y-8">
-        <div>
-            <flux:heading size="xl" class="mb-4">{{ __('api_clients.new_title') }}</flux:heading>
-            <flux:text class="text-base">{{ __('api_clients.explanation') }}</flux:text>
-        </div>
+    <div>
+        <x-livewire-form class="space-y-8">
+            <div>
+                <flux:heading size="xl" class="mb-4">{{ __('api_clients.new_title') }}</flux:heading>
+                <flux:text class="text-base">{{ __('api_clients.explanation') }}</flux:text>
+            </div>
 
-        <flux:field>
-            <flux:label>{{ __('api_clients.name') }}</flux:label>
-            <flux:input wire:model="name" placeholder="{{ __('api_clients.name_placeholder') }}" />
-        </flux:field>
+            <flux:field>
+                <flux:label>{{ __('api_clients.name') }}</flux:label>
+                <flux:input wire:model="name" placeholder="{{ __('api_clients.name_placeholder') }}" />
+            </flux:field>
 
-        <flux:field>
-            <flux:label>{{ __('api_clients.scopes') }}</flux:label>
-            <flux:checkbox.group wire:model="scopes">
-                <flux:checkbox value="committees" label="{{ __('api_clients.scope_committees') }}" />
-                <flux:checkbox value="groups" label="{{ __('api_clients.scope_groups') }}" />
-                <flux:checkbox value="users" label="{{ __('api_clients.scope_users') }}" />
-            </flux:checkbox.group>
-        </flux:field>
+            <flux:field>
+                <flux:label>{{ __('api_clients.scopes') }}</flux:label>
+                <flux:checkbox.group wire:model="scopes">
+                    <flux:checkbox value="committees" label="{{ __('api_clients.scope_committees') }}" />
+                    <flux:checkbox value="groups" label="{{ __('api_clients.scope_groups') }}" />
+                    <flux:checkbox value="users" label="{{ __('api_clients.scope_users') }}" />
+                </flux:checkbox.group>
+            </flux:field>
 
-        <x-slot:abort_route>
-            {{ route('realms.api-clients', ['realm' => $uid]) }}
-        </x-slot:abort_route>
-    </x-livewire-form>
+            <x-slot:abort_route>
+                {{ route('realms.api-clients', ['realm' => $uid]) }}
+            </x-slot:abort_route>
+        </x-livewire-form>
+    </div>
 @endif
