5c4da2859f994cbf5081d97d43dc08d05f43e5f0
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-18 16:00:50 +0200

    Update verify email view

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -9,7 +9,7 @@
         <flux:card class="grid gap-6 w-full bg-zinc-50 dark:bg-zinc-900 sm:bg-white sm:dark-bg-zinc-800 max-w-[28rem]! mx-auto border-0 sm:border-1 sm:shadow-xs">
             <div>{{ __('auth.verification_text') }}</div>
 
-            <div class="flex flex-wrap gap-2 items-center justify-between">
+            <div class="flex flex-wrap gap-2 items-center justify-end">
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     <flux:button
