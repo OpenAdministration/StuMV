ce39abd848a5e80a2af4553a4ae39d9145cc1658
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-22 02:05:19 +0200

    Update community dashboard

--- resources/views/livewire/realm/community-dashboard.blade.php
+++ resources/views/livewire/realm/community-dashboard.blade.php
@@ -26,7 +26,7 @@
                 class="flex hover:ring-2 focus:ring-2 ring-(--color-accent-content) rounded-lg"
             >
                 <div class="pt-4 px-3 bg-zinc-100 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 text-(--color-accent-content) rounded-l-lg">
-                    <flux:icon.network class="size-5" />
+                    <flux:icon.list-tree class="size-5" />
                 </div>
                 <flux:card size="sm" class="flex-1 rounded-l-none border-l-0 p-3">
                     <flux:heading size="lg">{{ __('realms.dashboard.committee_headline', ['name' => $name]) }}</flux:heading>
