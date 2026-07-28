7b7b2d3922c67fb857192731375b99e37e356dda
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-28 18:09:42 +0200

    text-sm

--- resources/views/auth/oauth/authorize.blade.php
+++ resources/views/auth/oauth/authorize.blade.php
@@ -21,7 +21,7 @@
                     @endif
                 </div>
                 @if($client->description)
-                    <p class="text-zinc-600 dark:text-zinc-300">{{ $client->description }}</p>
+                    <p class="text-sm text-zinc-600 dark:text-zinc-300">{{ $client->description }}</p>
                 @endif
             </div>
 
