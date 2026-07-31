ab56e94c024ff1b33893ea348886b49db868d69a
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-07-31 19:20:53 +0200

    ml-auto

--- resources/views/livewire/oidc/edit-oidc-client-logo.blade.php
+++ resources/views/livewire/oidc/edit-oidc-client-logo.blade.php
@@ -4,7 +4,7 @@
     @if($logoId)
         <div class="flex items-center gap-4">
             <img class="w-24 h-24 p-2 object-contain rounded-md border border-zinc-200 dark:border-zinc-700" src="{{ asset('storage/oidc-client-logos/'.$logoId) }}" alt="{{ __('oidc_clients.logo') }}">
-            <flux:button variant="danger" icon="trash-2" wire:click="removeLogo">{{ __('oidc_clients.remove_logo') }}</flux:button>
+            <flux:button variant="danger" icon="trash-2" wire:click="removeLogo" class="ml-auto">{{ __('oidc_clients.remove_logo') }}</flux:button>
         </div>
     @else
         <flux:file-upload wire:model="logo" accept="image/*">
